00982dd9392e4d1f99bdf6102f1a48a60e5f144f
mherreray <mherreray@alumnos.unex.es>
2026-03-10 20:45:13 +0100

    deploy

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  isQrOpen = false;
 
+  openQr(): void {
+    this.isQrOpen = true;
+  }
+
+  closeQr(): void {
+    this.isQrOpen = false;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    this.closeQr();
+  }
 }
