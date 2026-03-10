6869311ad1595b43a486cd21417a31bfeb43dbf2
mherreray <mherreray@alumnos.unex.es>
2026-03-10 18:06:17 +0100

    Add in-memory scrolling to router and implement booking section navigation

--- src/app/info/info.ts
+++ src/app/info/info.ts
@@ -1,4 +1,5 @@
 import { Component, AfterViewInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { ScrollAnimationService } from '../services/scroll-animation.service';
 
 @Component({
@@ -9,8 +10,13 @@ import { ScrollAnimationService } from '../services/scroll-animation.service';
 })
 export class Info implements AfterViewInit {
   private scrollAnimationService = inject(ScrollAnimationService);
+  private router = inject(Router);
 
   ngAfterViewInit() {
     this.scrollAnimationService.observeElements('.info-card');
+  }
+
+  goToBookingSection() {
+    this.router.navigate(['/planificacion-reserva'], { fragment: 'booking-section' });
   }
 }
